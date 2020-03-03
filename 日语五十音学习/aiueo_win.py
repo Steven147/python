4380214bd68538efcb0d3a840fae7cd9f357656c
LinShaoQin <857119585@qq.com>
2020-03-03 20:29:55 +0800

    version 2.0

--- 日语五十音学习/aiueo_win.py
+++ 日语五十音学习/aiueo_win.py
@@ -79,7 +79,7 @@ def botton4():
 #窗口初始化
 windows = tkinter.Tk()
 windows.title('aiueo')
-windows.geometry('400x600')
+windows.geometry('350x600')
 #文本框一初始化 显示字母组合
 var = tkinter.StringVar()
 l = tkinter.Label(windows, textvariable = var, font=('Arial, 30'), width=75, height=2)
@@ -89,20 +89,20 @@ var2 = tkinter.StringVar()
 l2 = tkinter.Label(windows, textvariable = var2, font=('Arial, 30'), width=75, height=2)
 l2.pack()
 #第一个按键 产生新的组合
-b = tkinter.Button(windows, text = 'NEW!', font=('Arial, 30'), width = 20, height=3, command=botton1)
+b = tkinter.Button(windows, text = 'NEW!', font=('Arial, 30'), width = 20, height=2, command=botton1)
 b.pack()
 #第二个按键 显示答案
-b2 = tkinter.Button(windows, text = "Answer?", font=('Arial, 30'), width = 20, height=3, command=botton2)
+b2 = tkinter.Button(windows, text = "Answer?", font=('Arial, 30'), width = 20, height=2, command=botton2)
 b2.pack()
 #第三个按键 开关答案显示和隐藏
 var3 = tkinter.StringVar()
 var3.set('Learning')
-b3 = tkinter.Button(windows, textvariable = var3, font=('Arial, 30'), width = 20, height=3, command=botton3)
+b3 = tkinter.Button(windows, textvariable = var3, font=('Arial, 30'), width = 20, height=2, command=botton3)
 b3.pack()
 #第三个按键 开关答案显示和隐藏
 var4 = tkinter.StringVar()
 var4.set('あ＋ア')
-b4 = tkinter.Button(windows, textvariable = var4, font=('Arial, 30'), width = 20, height=3, command=botton4)
+b4 = tkinter.Button(windows, textvariable = var4, font=('Arial, 30'), width = 20, height=2, command=botton4)
 b4.pack()
 windows.mainloop()
 
